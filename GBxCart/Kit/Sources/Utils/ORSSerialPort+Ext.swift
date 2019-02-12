5735ab7e12cd1fb0f6bfbead9ac65295ed1d21ee
Kevin Vitale <kevinvitale@gmail.com>
2019-02-11 23:16:02 -0500

    [Util] adds 'GBxCart' & 'PortProfile'

--- GBxCart/Kit/Sources/Utils/ORSSerialPort+Ext.swift
+++ GBxCart/Kit/Sources/Utils/ORSSerialPort+Ext.swift
@@ -1,15 +1,26 @@
 import ORSSerial
 
 extension ORSSerialPortManager {
-    static func port(matching prefix: String) -> ORSSerialPort? {
+    public enum PortProfile {
+        case prefix(String)
+
+        fileprivate func matcher() -> ((ORSSerialPort) -> Bool) {
+            switch self {
+            case .prefix(let prefix):
+                return { $0.path.hasPrefix(prefix) }
+            }
+        }
+    }
+    
+    private static func port(matching profile: PortProfile) -> ORSSerialPort? {
         return shared()
             .availablePorts
-            .filter({ $0.path.hasPrefix(prefix) })
+            .filter(profile.matcher())
             .first
     }
     
-    public static func GBxCart() -> ORSSerialPort? {
-        let port = ORSSerialPortManager.port(matching: "/dev/cu.usbserial-14")
+    public static func GBxCart(_ profile: PortProfile = .prefix("/dev/cu.usbserial-14")) -> ORSSerialPort? {
+        let port = ORSSerialPortManager.port(matching: profile)
         port?.allowsNonStandardBaudRates = true
         port?.baudRate = 1000000
         port?.dtr = true
