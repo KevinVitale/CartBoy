a855a0ea32d3829e2f7ea85ac377415b86a1402a
Kevin Vitale <kevinvitale@gmail.com>
2019-02-15 20:22:49 -0500

    [Tests] adds 'openReader(matching:)' in base

--- GBxCart/Tests/Util/BaseReadROMTest.swift
+++ GBxCart/Tests/Util/BaseReadROMTest.swift
@@ -7,6 +7,19 @@ open class BaseReadROMTest<Gameboy: Platform>: XCTestCase {
     private(set) var reader: ORSSerialPort!
     private let queue = OperationQueue()
     
+    final func openReader(matching profile: ORSSerialPortManager.PortProfile) throws {
+        guard reader.isOpen == false else {
+            return
+        }
+        self.reader = try ORSSerialPortManager.port(matching: profile)
+        
+        switch profile {
+        case .GBxCart:
+            self.reader = self.reader.configuredAsGBxCart()
+        default: ()
+        }
+    }
+
     final func read<Result: PlatformMemory>(rom memoryRange: ReadROMOperation<Gameboy>.MemoryRange, result: @escaping ((Result?) -> ())) where Result.Platform == Gameboy {
         queue.addOperation(operation(for: memoryRange, result: result))
     }
