89d358a63d2c525bbe05ad516280a9957cd910a2
Kevin Vitale <kevinvitale@gmail.com>
2019-02-15 19:50:47 -0500

    [Tests] adds 'read()' convenience function

--- GBxCart/Tests/Util/BaseReadROMTest.swift
+++ GBxCart/Tests/Util/BaseReadROMTest.swift
@@ -5,8 +5,15 @@ import GBxCartKit
 
 open class BaseReadROMTest<Gameboy: Platform>: XCTestCase {
     private(set) var reader: ORSSerialPort!
+    private let queue = OperationQueue()
     
-    func operation<Result: PlatformMemory>(for memoryRange: ReadROMOperation<Gameboy>.MemoryRange, result: @escaping ((Result?) -> ())) -> ReadROMOperation<Gameboy> where Result.Platform == Gameboy {
+    final func read<Result: PlatformMemory>(rom memoryRange: ReadROMOperation<Gameboy>.MemoryRange, result: @escaping ((Result?) -> ())) where Result.Platform == Gameboy {
+        queue.addOperation(operation(for: memoryRange, result: result))
+    }
+}
+
+extension BaseReadROMTest {
+    fileprivate final func operation<Result: PlatformMemory>(for memoryRange: ReadROMOperation<Gameboy>.MemoryRange, result: @escaping ((Result?) -> ())) -> ReadROMOperation<Gameboy> where Result.Platform == Gameboy {
         return ReadROMOperation(device: reader, memoryRange: memoryRange, cleanup: result)
     }
 }
