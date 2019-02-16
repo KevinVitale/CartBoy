83b87a95e32e50c1ac11d5f52b3778070feeb1f3
Kevin Vitale <kevinvitale@gmail.com>
2019-02-15 20:36:00 -0500

    [GBxCart] adds 'ReaderController' protocol

---
+++ GBxCart/Kit/Sources/Controller/ReaderController.swift
@@ -0,0 +1,18 @@
+import ORSSerial
+import Gibby
+
+public protocol ReaderController {
+    associatedtype Platform = Gibby.Platform
+    
+    var reader: ORSSerialPort! { get }
+    
+    func openReader(matching profile: ORSSerialPortManager.PortProfile) throws
+    func read<Result: PlatformMemory>(rom memoryRange: ReadROMOperation<Platform>.MemoryRange, result: @escaping ((Result?) -> ())) where Result.Platform == Platform
+}
+
+extension ReaderController {
+    public func operation<Result: PlatformMemory>(for memoryRange: ReadROMOperation<Platform>.MemoryRange, result: @escaping ((Result?) -> ())) -> ReadROMOperation<Platform> where Result.Platform == Platform {
+        return ReadROMOperation(device: reader, memoryRange: memoryRange, cleanup: result)
+    }
+}
+
