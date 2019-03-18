cd887168f8d0671fe24fb0b1ada18f963e6119d6
Kevin Vitale <kevinvitale@gmail.com>
2019-03-18 17:23:15 -0400

    [GBxCart] ‘header’ get an override

--- CartBoyKit/Source/Devices/GBxCart/GBxCartridgeController.swift
+++ CartBoyKit/Source/Devices/GBxCart/GBxCartridgeController.swift
@@ -8,3 +8,15 @@ import Gibby
  */
 public class GBxCartridgeController<Cartridge: Gibby.Cartridge>: GBxSerialPortController, CartridgeController {
 }
+
+extension GBxCartridgeController {
+    public func header(result: @escaping ((Cartridge.Header?) -> ())) {
+        self.addOperation(SerialPacketOperation(controller: self, delegate: self, intent: .read(count: Cartridge.Platform.headerRange.count, context: OperationContext.header)) {
+            guard let data = $0 else {
+                result(nil)
+                return
+            }
+            result(.init(bytes: data))
+        })
+    }
+}
