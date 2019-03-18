b39058db847775bb1bce3a8c93a6d7d1f846d9cc
Kevin Vitale <kevinvitale@gmail.com>
2019-03-18 10:17:50 -0400

    [CBY] Adds 'CartBoy' target
    It begins...

---
+++ CartBoy/CartBoy/Document.swift
@@ -0,0 +1,27 @@
+//
+//  Document.swift
+//  CartBoy
+//
+//  Created by Kevin Vitale on 3/18/19.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override var windowNibName: NSNib.Name? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return NSNib.Name("Document")
+    }
+
+}
---
+++ CartBoy/CartBoyTests/CartBoyTests.swift
@@ -0,0 +1,33 @@
+//
+//  CartBoyTests.swift
+//  CartBoyTests
+//
+//  Created by Kevin Vitale on 3/18/19.
+//
+
+import XCTest
+@testable import CartBoy
+
+class CartBoyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
