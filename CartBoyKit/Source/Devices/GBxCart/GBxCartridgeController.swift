df35ef9a2e11750bcb18bae2644eeab14e4de1ba
Kevin Vitale <kevinvitale@gmail.com>
2019-03-18 22:43:53 -0400

    [GBxCart] adds private ‘header’ variable

--- CartBoyKit/Source/Devices/GBxCart/GBxCartridgeController.swift
+++ CartBoyKit/Source/Devices/GBxCart/GBxCartridgeController.swift
@@ -7,13 +7,16 @@ import Gibby
  platform-specific serial port operations.
  */
 public class GBxCartridgeController<Cartridge: Gibby.Cartridge>: GBxSerialPortController, CartridgeController {
+    private(set) var header: Cartridge.Header? = nil
+    
     public func header(result: @escaping ((Cartridge.Header?) -> ())) {
-        self.addOperation(SerialPacketOperation(controller: self, delegate: self, intent: .read(count: Cartridge.Platform.headerRange.count, context: OperationContext.header)) {
+        self.addOperation(SerialPacketOperation(controller: self, delegate: self, intent: .read(count: Cartridge.Platform.headerRange.count, context: OperationContext.header)) { [weak self] in
             guard let data = $0 else {
                 result(nil)
                 return
             }
-            result(.init(bytes: data))
+            self?.header = .init(bytes: data)
+            result(self?.header)
         })
     }
 
