868a1a297354cacf28ca7819f178a53283373070
Kevin Vitale <kevinvitale@gmail.com>
2019-03-26 09:36:53 -0400

    [IGC] fixes grammar

--- CartBoyKit/Source/Devices/InsideGadgets/insideGadgetsReader.swift
+++ CartBoyKit/Source/Devices/InsideGadgets/insideGadgetsReader.swift
@@ -5,21 +5,21 @@ public struct InsideGadgetsReader<Cartridge: Gibby.Cartridge>: CartridgeReader, 
     }
     
     public func readHeader<Controller>(using controller: Controller, result: @escaping (Cartridge.Header?) -> ()) -> Operation where Controller: SerialPortController {
-        fatalError("Controller does not platform: \(Cartridge.Platform.self)")
+        fatalError("Controller does not support platform: \(Cartridge.Platform.self)")
     }
     public func readCartridge<Controller>(using controller: Controller, with header: Cartridge.Header?, result: @escaping (Cartridge?) -> ()) -> Operation where Controller : SerialPortController {
-        fatalError("Controller does not platform: \(Cartridge.Platform.self)")
+        fatalError("Controller does not support platform: \(Cartridge.Platform.self)")
     }
     
     public func backupSave<Controller>(using controller: Controller, with header: Cartridge.Header?, result: @escaping (Data?) -> ()) -> Operation where Controller : SerialPortController {
-        fatalError("Controller does not platform: \(Cartridge.Platform.self)")
+        fatalError("Controller does not support platform: \(Cartridge.Platform.self)")
     }
     
     public func restoreSave<Controller>(data: Data, using controller: Controller, with header: Cartridge.Header?, result: @escaping (Bool) -> ()) -> Operation where Controller : SerialPortController {
-        fatalError("Controller does not platform: \(Cartridge.Platform.self)")
+        fatalError("Controller does not support platform: \(Cartridge.Platform.self)")
     }
     
     public func deleteSave<Controller>(using controller: Controller, with header: Cartridge.Header?, result: @escaping (Bool) -> ()) -> Operation where Controller : SerialPortController {
-        fatalError("Controller does not platform: \(Cartridge.Platform.self)")
+        fatalError("Controller does not support platform: \(Cartridge.Platform.self)")
     }
 }
