d3c0c93679c19801b7c0122d2a1ec17b8c74aa3f
Kevin Vitale <kevinvitale@gmail.com>
2019-04-01 11:15:00 -0400

    [CartBoy] new targets (now w/more StoryBoards!)

---
+++ CartBoy/CartBoy/Document.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+class Document: NSPersistentDocument {
+    override init() {
+        super.init()
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+        self.addWindowController(windowController)
+    }
+}
---
+++ CartBoy/CartBoy/ViewController.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+class ViewController: NSViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+}
