16551f52cdd231e92f5f8cbeb3240460cccb0017
Kevin Vitale <kevinvitale@gmail.com>
2019-04-02 08:49:12 -0400

    [CartArchiver] adds a couple of handy comments

--- CartBoyKit/Source/Core/Cartridge/CartridgeArchiver.swift
+++ CartBoyKit/Source/Core/Cartridge/CartridgeArchiver.swift
@@ -1,5 +1,17 @@
 import Gibby
 
+/**
+ Describes an interface that sends or receives save data to and from cartridges.
+
+ Archivers are capable of:
+ 
+    - _backing_ up save data __from__,
+    - _restoring_ save data __to__, and
+    - _deleting_ save data __off of__
+ 
+ cartridges that support such hardware necessary to store save data.
+ */
+@available(macOS 10.11, *)
 public protocol CartridgeArchiver {
     associatedtype Cartridge: Gibby.Cartridge
     func backupSave(with header: Cartridge.Header?, result: @escaping (Data?) -> ())
