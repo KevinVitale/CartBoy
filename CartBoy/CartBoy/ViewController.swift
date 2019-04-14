5390d98437d44cae0b9ed06a29ab22c792f14297
Kevin Vitale <kevinvitale@gmail.com>
2019-04-14 12:46:26 -0400

    [UI] adds buttons and layout titles

--- CartBoy/CartBoy/ViewController.swift
+++ CartBoy/CartBoy/ViewController.swift
@@ -15,13 +15,38 @@ class ViewController: NSViewController {
 }
 
 class ContentViewController: NSViewController {
+    @IBOutlet weak var cartridgeProgressBar: NSProgressIndicator!
+    
     @IBAction func readHeader(_ sender: Any?) {
         print(#function)
         do {
-            let reader = try InsideGadgetsCartridgeController.reader(for: GameboyClassic.Cartridge.self)
-            reader.readHeader {
-                if let header = $0 {
+            let reader = try InsideGadgetsCartridgeController.reader(for: GameboyClassic.Cartridge.self).get()
+            reader.header {
+                switch $0 {
+                case .failure(let error):
+                    print(error)
+                case .success(let header):
                     print(header)
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func readCartridge(_ sender: Any?) {
+        print(#function)
+        do {
+            let reader = try InsideGadgetsCartridgeController.reader(for: GameboyClassic.Cartridge.self).get()
+            reader.cartridge(progress: { [weak self] in
+                self?.cartridgeProgressBar.doubleValue = $0
+            }) {
+                switch $0 {
+                case .failure(let error):
+                    print(error)
+                case .success(let cartridge):
+                    print(cartridge)
+                    print(cartridge.header)
                 }
             }
         } catch {
