1a920da022cfc72954e28ae881f1fc26fd378ba6
Kevin Vitale <kevinvitale@gmail.com>
2019-05-28 11:19:40 -0400

    [Cart] controller errors now display correctly

--- CartKit/Source/Cartridge/CartridgeController.swift
+++ CartKit/Source/Cartridge/CartridgeController.swift
@@ -17,9 +17,34 @@ public protocol CartridgeController: SerialPortController {
     func erase<FlashCartridge: CartKit.FlashCartridge>(chipset: FlashCartridge.Type) -> Result<(), Error>
 }
 
-public enum CartridgeControllerError<Platform: Gibby.Platform>: Error {
+public enum CartridgeControllerError<Platform: Gibby.Platform>: LocalizedError, CustomNSError {
     case platformNotSupported(Platform.Type)
     case invalidHeader
+    
+    public var errorDescription: String? {
+        switch self {
+        case .platformNotSupported(let platform): return NSLocalizedString("\(platform) not supported".capitalized, comment: "")
+        case .invalidHeader: return NSLocalizedString("Invalid Cartridge Header", comment: "")
+        }
+    }
+
+    public var recoverySuggestion: String? {
+        switch self {
+        case .platformNotSupported: return nil
+        case .invalidHeader: return NSLocalizedString("The cartridge's header is either missing or otherwise not valid. Reseat the cartridge in the device and try again.", comment: "")
+        }
+    }
+    
+    public var errorCode: Int {
+        switch self {
+        case .platformNotSupported: return 111
+        case .invalidHeader: return 101
+        }
+    }
+    
+    public static var errorDomain: String {
+        return "com.cartboy.controller.cartridge"
+    }
 }
 
 public enum CartridgeFlashError<FlashCartridge: CartKit.FlashCartridge>: Error {
